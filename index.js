455be3bb2871661c1ddb6299d81ab017cf97a288
Harsh-Muni2023 <161803762+Harsh-Muni2023@users.noreply.github.com>
2024-06-26 08:42:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,3 +24,19 @@ undergradToggle.addEventListener("click",() =>{
         display2.classList.replace("undergraduate-year","undergraduate");
     }
 })
+
+let form = document.getElementById("contactform");
+let contact = document.getElementById("contact-me");
+let sub = document.getElementById("cut");
+
+contact.addEventListener("click",() =>{
+    if(form.classList.contains("contactform")){
+        form.classList.replace("contactform","contactform-real");
+    }
+})
+
+sub.addEventListener("click",() =>{
+    if(form.classList.contains("contactform-real")){
+        form.classList.replace("contactform-real","contactform");
+    }
+})
